6e2a9b8cfaf4dcb6454c23151cfc7ce75468f818
Harith Siddhartha <harith.s@gmail.com>
2012-09-29 15:06:29 +0530

    Deleted CSS file. Switched to inline styling using javascript inorder to override webpage styles.
    Added a buffer between the click position and the position at which
    oald-link is spawned. Otherwise, when a user tries to triple click,
    the third click will be triggered on oald-link.

--- js/oald.js
+++ js/oald.js
@@ -1,24 +1,69 @@
 (function() {
 
     var DEFAULT_DISPLAY_STYLE = 'inline-block',
+        POSITION_BUFFER       = 5, // in pixels
         oaldBaseURL = 'http://oald8.oxfordlearnersdictionaries.com/dictionary/',
         link        = document.createElement('a'),
         icon        = document.createElement('img'),
         helperText  = document.createElement('span');
 
-    /* Utility functions */
+    var CSSObject = function(style) {
+        this.style = style;
+        this.inlineCSSText = function() {
+            var cssText = JSON.stringify(this.style);
+            return cssText.replace(/["}{]/g,'').replace(/,/g,';');
+        }
+    };
+
+    /* To add inline CSS instead of CSS from a file. 
+     * Inline CSS will override all other styles. */
+    var linkStyle = new CSSObject({
+            "margin"            : "0",
+            "padding"           : "0",
+            "position"          : "absolute",
+            "display"           : "none",
+            "text-decoration"   : "none",
+            "padding"           : "0.25em",
+            "border"            : "1px solid black",
+            "border-radius"     : "2px",
+            "background"        : "white",
+            "font-size"         : "12px",
+            "color"             : "black",
+            "font-family"       : "sans-serif"
+        }),
+        linkIconStyle = new CSSObject({
+            "margin"         : "0",
+            "padding"        : "0",
+            "vertical-align" : "middle"
+        }),
+        helperTextStyle = new CSSObject({
+            "margin"      : "0",
+            "padding"     : "0",
+            "margin-left" : "0.25em",
+            "display"     : "none"
+        });
+
+    /* Utility functions 
+     *
+     * show, hide: These functions return functions so that 
+     *             they can be used as event listeners
+     */
     var hide = function(element) {
-            return function() { element.style.display = 'none' };
+            return function() { console.log("hiding " + element); element.style.display = 'none' };
         },
         show = function(element, displayType) {
-            return function() { element.style.display = displayType || DEFAULT_DISPLAY_STYLE };
+            return function() { console.log("showing " + element); element.style.display = displayType || DEFAULT_DISPLAY_STYLE };
         };
+     
+    link.id             = 'oald-link';
+    link.target         = 'new';
+    link.style.cssText  = linkStyle.inlineCSSText();
 
-    icon.src = chrome.extension.getURL('oald.ico');
-    helperText.textContent = 'see meaning';
+    icon.src            = chrome.extension.getURL('oald.ico');
+    icon.style.cssText  = linkIconStyle.inlineCSSText();
 
-    link.id          = 'oald-link';
-    link.target      = 'new';
+    helperText.textContent   = 'see meaning';
+    helperText.style.cssText = helperTextStyle.inlineCSSText();
 
     link.appendChild(icon);
     link.appendChild(helperText);
@@ -33,8 +78,8 @@
         var selection = window.getSelection().toString().trim();
         if ( selection !== '' ) {
             link.href = oaldBaseURL + selection;
-            link.style.left = e.pageX + 'px';
-            link.style.top  = e.pageY + 'px';
+            link.style.left = e.pageX + POSITION_BUFFER + 'px';
+            link.style.top  = e.pageY + POSITION_BUFFER + 'px';
             show(link).call();
         }
     });

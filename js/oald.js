2527ce2f3f8a52bfccbb231679dcfa6ea995b533
Harith Siddhartha <harith.s@gmail.com>
2012-09-30 20:29:41 +0530

    - Added XHR calls to oald page to fetch word definitions. - Only first 3 definitions of a word will be displayed along with pronunciation. - Definitions are shown at the bottom of the page in a 'fixed' div. - User can still click on the link in this div to visit oald page.
    renamed:    oald.ico -> img/oald.ico

--- js/oald.js
+++ js/oald.js
@@ -2,10 +2,13 @@
 
     var DEFAULT_DISPLAY_STYLE = 'inline-block',
         POSITION_BUFFER       = 5, // in pixels
+        RESET       = '<a href="#" target="new" id="oald-external"></a>',
+        MAX_DEFS    = 3,
         oaldBaseURL = 'http://oald8.oxfordlearnersdictionaries.com/dictionary/',
         link        = document.createElement('a'),
         icon        = document.createElement('img'),
         helperText  = document.createElement('span');
+        definitions = document.createElement('div');
 
     /* Utility functions 
      *
@@ -18,6 +21,9 @@
         show = function(element, displayType) {
             return function() { element.style.display = displayType || DEFAULT_DISPLAY_STYLE };
         },
+        minimum = function(a, b) {
+            return a < b ? a : b;
+        },
         CSSObject = function(style) {
             this.style = style;
             this.inlineCSSText = function() {
@@ -27,19 +33,34 @@
         };
 
     /* OALD call */
-    var getWordDefinitions = function(e) {
+    var getWordDefinitions = function(url) {
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", this.href);
+        xhr.open("GET", url);
         xhr.onreadystatechange = function() {
             if ( xhr.readyState == 4 ) {
-                var div = document.createElement('div');
-                div.innerHTML = xhr.responseText;
-                var defs = div.querySelectorAll('.d');
-                for ( i in defs ) { console.log(defs[i].textContent); }
+                definitions.innerHTML = RESET;
+                var oaldLink = definitions.querySelector("#oald-external"),
+                    root     = document.createElement("div"),
+                    defs, iterations = 0, pronunciation;
+
+                root.innerHTML = xhr.responseText;
+                defs = root.querySelectorAll(".d");
+                pronunciation = ' (' + root.querySelector(".i").textContent + ' )';
+
+                oaldLink.style.display = "block";
+                oaldLink.href = url;
+                oaldLink.textContent = url.split('/').pop() + pronunciation;
+
+                iterations = minimum( defs.length, MAX_DEFS );
+                for ( var i = 0; i < iterations; i++ ) {
+                    defs[i].style.display = "block";
+                    definitions.appendChild(defs[i]);
+                }
+                show(definitions).call();
+                hide(link).call(); 
             }
         };
         xhr.send();
-        e.preventDefault();
     }
 
     /* To add inline CSS instead of CSS from a file. 
@@ -70,41 +91,70 @@
             "padding"     : "0",
             "margin-left" : "0.25em",
             "display"     : "none"
+        }),
+        definitionsStyle = new CSSObject({
+            "display"     : "none",
+            "position"    : "fixed",
+            "background"  : "white",
+            "color"       : "black",
+            "font-size"   : "16px",
+            "font-family" : "sans-serif",
+            "z-index"     : "9999",
+            "bottom"      : "0",
+            "left"        : "0",
+            "width"       : "100%",
+            "border-top"  : "1px solid black",
+            "padding"     : "2em",
+            "text-align"  : "left",
+            "background-color" : "#F0F0F0"
         });
 
     link.id             = 'oald-link';
     link.target         = 'new';
     link.style.cssText  = linkStyle.inlineCSSText();
-    link.addEventListener('click', getWordDefinitions)
 
-    icon.src            = chrome.extension.getURL('oald.ico');
     icon.style.cssText  = iconStyle.inlineCSSText();
 
     helperText.textContent   = 'see meaning';
     helperText.style.cssText = helperTextStyle.inlineCSSText();
 
+    definitions.id              = 'oald-definitions';
+    definitions.style.cssText   = definitionsStyle.inlineCSSText();
+
     link.appendChild(icon);
     link.appendChild(helperText);
 
-    link.addEventListener( 'click', hide(link) );
     link.addEventListener( 'mouseover', show(helperText) );
     link.addEventListener( 'mouseout',  hide(helperText) );
 
     document.body.appendChild(link);
+    document.body.appendChild(definitions);
 
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        icon.src = chrome.extension.getURL('img/spinner.gif');
+        helperText.textContent = 'fetching…';
+        getWordDefinitions(this.href);
+    });
     document.addEventListener('dblclick', function(e) {
         var selection = window.getSelection().toString().trim();
         if ( selection !== '' ) {
-            link.href = oaldBaseURL + selection;
+            helperText.textContent = 'see meaning';
+            icon.src        = chrome.extension.getURL('img/oald.ico');
+            link.href       = oaldBaseURL + selection;
             link.style.left = e.pageX + POSITION_BUFFER + 'px';
             link.style.top  = e.pageY + POSITION_BUFFER + 'px';
             show(link).call();
         }
     });
-
-    /* Clicking anywhere else in the page removes selection.
-     * We don't want OALD link to be shown after the selection
-     * is removed. */
-    document.addEventListener( 'click', hide(link) );
+    document.addEventListener( 'click', function(e) {
+        /* Clicking anywhere else in the page removes selection.
+         * We don't want OALD link to be shown after the selection
+         * is removed. */
+        if ( e.target.parentNode.id !== 'oald-link' ) {
+            hide(link).call(); 
+            hide(definitions).call();
+        }
+    });
 
 })();
